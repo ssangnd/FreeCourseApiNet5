523a13a0da6c2975dcb22b4f8ec66b9e729b0b5b
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-11-12 22:02:19 +0700

    add PaginatedList

--- Services/HangHoaRepository.cs
+++ Services/HangHoaRepository.cs
@@ -1,5 +1,6 @@
 ﻿using FreeCourseApiNet5.Data;
 using FreeCourseApiNet5.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -14,9 +15,10 @@ namespace FreeCourseApiNet5.Services
         {
             _context = context;
         }
-        public List<HangHoaModel> GetAll(string search, double? from, double? to, string sortBy,int page)
+        public List<HangHoaModel> GetAll(string search, double? from, double? to, string sortBy, int page)
         {
-            var hanghoas = _context.HangHoas.AsQueryable();
+            //var hanghoas = _context.HangHoas.AsQueryable(); //test error pagedList
+            var hanghoas = _context.HangHoas.Include(hh=>hh.Loai).AsQueryable();
 
             #region Filtering
             if (!string.IsNullOrEmpty(search))
@@ -32,6 +34,7 @@ namespace FreeCourseApiNet5.Services
             {
                 hanghoas = hanghoas.Where(hh => hh.DonGia <= to);
             }
+            #endregion
 
             #region Sorting
             //default sort by Name (TenHH)
@@ -40,7 +43,8 @@ namespace FreeCourseApiNet5.Services
             {
                 switch (sortBy)
                 {
-                    case "tenhh_desc": hanghoas = hanghoas.OrderByDescending(hh => hh.TenHh);
+                    case "tenhh_desc":
+                        hanghoas = hanghoas.OrderByDescending(hh => hh.TenHh);
                         break;
                     case "gia_asc":
                         hanghoas = hanghoas.OrderBy(hh => hh.DonGia);
@@ -52,20 +56,30 @@ namespace FreeCourseApiNet5.Services
             }
             #endregion
 
-            #region Paging
-            hanghoas=hanghoas.Skip((page-1)*PAGE_SIZE).Take(PAGE_SIZE);
-            #endregion
+            // Paging
+            //hanghoas = hanghoas.Skip((page - 1) * PAGE_SIZE).Take(PAGE_SIZE);
+            
 
-            var result = hanghoas.Select(hh => new HangHoaModel
+            //var result = hanghoas.Select(hh => new HangHoaModel
+            //{
+            //    MaHangHoa = hh.MaHh,
+            //    TenHangHoa = hh.TenHh,
+            //    DonGia = hh.DonGia,
+            //    TenLoai = hh.Loai.TenLoai,
+            //});
+          
+
+            //return result.ToList();
+
+            var result = PaginatedList<FreeCourseApiNet5.Data.HangHoa>.Create(hanghoas, page, PAGE_SIZE);
+
+            return result.Select(hh => new HangHoaModel
             {
                 MaHangHoa = hh.MaHh,
                 TenHangHoa = hh.TenHh,
                 DonGia = hh.DonGia,
-                TenLoai = hh.Loai.TenLoai,
-            });
-            #endregion
-
-            return result.ToList();
-        }
+                TenLoai = hh.Loai?.TenLoai,
+            }).ToList();
+        } 
     }
 }
